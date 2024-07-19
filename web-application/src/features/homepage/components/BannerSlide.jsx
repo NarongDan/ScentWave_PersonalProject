e08ad696045ab9680001a7295f83e300e42683c3
NarongDan <narong.dan1@gmail.com>
2024-07-19 12:05:13 +0700

    move photos to public file

--- web-application/src/features/homepage/components/BannerSlide.jsx
+++ web-application/src/features/homepage/components/BannerSlide.jsx
@@ -1,10 +1,10 @@
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 
-import photo1 from "../../../assets/images/17223314_tp202-techi-blogbanner-06-pf-s50.jpg";
-import photo2 from "../../../assets/images/6993805_585.jpg";
-import photo3 from "../../../assets/images/perfume-banner-design-template-e0681a33c8dae68510dfef525c8fe42d_screen.jpg";
-import photo4 from "../../../assets/images/tender-womens-perfume-3d-realistic-vector-advertising-banner-poster-template_33099-1333.jpg";
+import photo1 from "../../../../public/assets/images/17223314_tp202-techi-blogbanner-06-pf-s50.jpg";
+import photo2 from "../../../../public/assets/images/6993805_585.jpg";
+import photo3 from "../../../../public/assets/images/perfume-banner-design-template-e0681a33c8dae68510dfef525c8fe42d_screen.jpg";
+import photo4 from "../../../../public/assets/images/tender-womens-perfume-3d-realistic-vector-advertising-banner-poster-template_33099-1333.jpg";
 
 export default function BannerSlide({ className }) {
   return (
